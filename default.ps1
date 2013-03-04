93ca94859bb5125f09927eef293e551721a3ca96
jonnii <me@jonnii.com>
2013-03-04 17:06:17 -0500

    add publish task.

--- default.ps1
+++ default.ps1
@@ -1,6 +1,6 @@
 Framework "4.0"
 
-task default -depends BuildPackage
+task default -depends Package
 
 task Clean {
 	$dir = '.\build'
@@ -9,8 +9,13 @@ task Clean {
     }
 }
 
-task BuildPackage -depends Clean {
+task Package -depends Clean {
 	mkdir build
 	$version = gc ./Version
 	.\tools\nuget.exe pack ".\package\package.nuspec" -outputdirectory ".\build" -version $version
+}
+
+task Publish -depends Package {
+	$package = gci .\build\*.nupkg | select -first 1
+	.\tools\nuget.exe Push $package.fullname
 }
