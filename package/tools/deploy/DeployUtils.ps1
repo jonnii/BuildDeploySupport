dd75e14794d84363141968190eb756f329349dd9
jonnii <me@jonnii.com>
2013-03-05 10:14:46 -0500

    add deploy utils

---
+++ package/tools/deploy/DeployUtils.ps1
@@ -0,0 +1,30 @@
+###################################################
+# DO NOT MODIFY THIS FILE, IT WILL BE OVERWRITTEN #
+###################################################
+
+# If you want to make changes please fork and contribute to the BuildDeploySupport 
+# project on github, so everyone can take advantage of your changes!
+
+function ReadXmlNode($filename, $xpath){
+    $xml = [xml](Get-Content $filename)
+	$node = $xml.SelectNodes($xpath) | select -f 1
+    
+    if(!($node)){
+        throw "could not find path $path"
+    }
+    
+    return $node.innertext
+}
+
+function WriteXmlAttribute($filename, $xpath, $name, $value){
+	$xml = [xml](Get-Content $filename)
+			
+	$node = $xml.SelectNodes(xpath) | select -f 1
+	
+	if(!($node)){
+        throw "could not find path $path"
+    }
+
+	$node.SetAttribute($name, $value)
+	$xml.save($filename)
+}
