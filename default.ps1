b48bb89a6943e8d491a705f9e5dfe8356ca85e68
jonnii <me@jonnii.com>
2013-03-06 17:13:53 -0500

    output to an output directory, not /build

--- default.ps1
+++ default.ps1
@@ -3,34 +3,34 @@ Framework "4.0"
 task default -depends Package
 
 task Clean {
-	$dir = '.\build'
+	$dir = '.\output'
 	if (test-path $dir) {
 		rm -force -recurse $dir
     }
 }
 
 task Package -depends Clean {
-	mkdir build
+	mkdir output
 	$version = gc ./Version
-	.\tools\nuget.exe pack ".\package\package.nuspec" -outputdirectory ".\build" -version $version
+	.\tools\nuget.exe pack ".\package\package.nuspec" -outputdirectory ".\output" -version $version
 }
 
 task PackagePre -depends Clean {
-	mkdir build
+	mkdir output
 
 	$version = gc ./Version
 	$when = (get-date).ToString("yyyyMMddHHmmss")
 	$preVersion = "$version-dev-$when"
 	
-	.\tools\nuget.exe pack ".\package\package.nuspec" -outputdirectory ".\build" -version $preVersion
+	.\tools\nuget.exe pack ".\package\package.nuspec" -outputdirectory ".\output" -version $preVersion
 }
 
 task Publish -depends Package {
-	$package = gci .\build\*.nupkg | select -first 1
+	$package = gci .\output\*.nupkg | select -first 1
 	.\tools\nuget.exe Push $package.fullname
 }
 
 task PublishPre -depends PackagePre {
-	$package = gci .\build\*.nupkg | select -first 1
+	$package = gci .\output\*.nupkg | select -first 1
 	.\tools\nuget.exe Push $package.fullname
 }
