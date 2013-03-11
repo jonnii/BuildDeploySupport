a287a3c85656f3c2f7961fb14d501ae093d47375
jonnii <me@jonnii.com>
2013-03-11 17:59:42 -0400

    tabs => spaces. concat provider path, don't use join-path.

--- package/tools/deploy/ClickOnce.ps1
+++ package/tools/deploy/ClickOnce.ps1
@@ -1,112 +1,125 @@
 function PrepareClickOnce() {
-	param(
-		[Parameter(Mandatory=$true)] [string] $target,
-		[Parameter(Mandatory=$true)] [string] $version,
-		[Parameter(Mandatory=$true)] [string] $source,
-		[Parameter(Mandatory=$true)] [string] $applicationExecutable,
-		[Parameter(Mandatory=$true)] [string] $applicationName,
-		[Parameter(Mandatory=$true)] [string] $applicationDisplayName,
-		[Parameter(Mandatory=$true)] [string] $icon,
-		[Parameter(Mandatory=$true)] [string] $publisher,
-		[Parameter(Mandatory=$true)] [string] $providerPath,
-		[string] $thumbprint
-	)
+  param(
+    [Parameter(Mandatory=$true)] [string] $target,
+    [Parameter(Mandatory=$true)] [string] $version,
+    [Parameter(Mandatory=$true)] [string] $source,
+    [Parameter(Mandatory=$true)] [string] $applicationExecutable,
+    [Parameter(Mandatory=$true)] [string] $applicationName,
+    [Parameter(Mandatory=$true)] [string] $applicationDisplayName,
+    [Parameter(Mandatory=$true)] [string] $icon,
+    [Parameter(Mandatory=$true)] [string] $publisher,
+    [Parameter(Mandatory=$true)] [string] $providerPath,
+    [string] $thumbprint
+  )
 
-	# Add mage to our path
-	$env:path += ";C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools"
+  Write-Host "Preparing click once"
+  Write-Host " -> Target: $target"
+  Write-Host " -> Version: $version"
+  Write-Host " -> Source: source"
+  Write-Host " -> Application Executable: $applicationExecutable"
+  Write-Host " -> Application Name: $applicationName"
+  Write-Host " -> Application Display Name: $applicationDisplayName"
+  Write-Host " -> Icon: $icon"
+  Write-Host " -> Publisher: $publisher"
+  Write-Host " -> ProviderPath: $providerPath"
+  Write-Host " -> Thumbprint: $thumbprint"
 
-    write-host "Preparing install directory"
-	if (test-path $target) {
-		Write-Host "Cleaning target directory"
-		rm -r -force $target > $null
-    }
-	mkdir $target > $null
+  # Add mage to our path
+  $env:path += ";C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools"
 
-	$targetVersion = join-path $target $version
-	
-	# create an install version directory
-	mkdir $targetVersion
-	write-host "Prepare install directory for version $version"
+  Write-Host "Preparing install directory"
+  if (test-path $target) {
+    Write-Host "Cleaning target directory"
+    rm -r -force $target > $null
+  }
+  mkdir $target > $null
 
-	cp -r -exclude *.xml "$source\*" $targetVersion
-	rm "$targetVersion\*vshost*"
+  $targetVersion = join-path $target $version
+  
+  # create an install version directory
+  mkdir $targetVersion
+  write-host "Prepare install directory for version $version"
 
-	write-host 'Creating manifest file'
-	$manifestFileName = join-path $targetVersion "$applicationExecutable.exe.manifest"
-	mage -New Application `
-		 -Processor x86 `
-		 -ToFile $manifestFileName `
-		 -name $applicationName `
-		 -Version $version `
-		 -FromDirectory $targetVersion `
-		 -IconFile $icon
+  cp -r -exclude *.xml "$source\*" $targetVersion
+  rm "$targetVersion\*vshost*"
 
-	# write-host ' -> Add file associations to manfest'
-	# $fullPath = resolve-path($manifestFileName)
-	# $doc = [xml](Get-Content -Path $fullPath)
-	# $association = $doc.CreateElement('fileAssociation')
-	# $association.SetAttribute('xmlns','urn:schemas-microsoft-com:clickonce.v1')
-	# $association.SetAttribute('extension','.ext')
-	# $association.SetAttribute('description','Decription')
-	# $association.SetAttribute('progid','YourApp.Document')
-	# $association.SetAttribute('defaultIcon', $icon)
-	# $doc.assembly.AppendChild($association) > $null
-	# $doc.Save($fullPath)
+  write-host 'Creating manifest file'
+  $manifestFileName = join-path $targetVersion "$applicationExecutable.exe.manifest"
+  mage -New Application `
+     -Processor x86 `
+     -ToFile $manifestFileName `
+     -name $applicationName `
+     -Version $version `
+     -FromDirectory $targetVersion `
+     -IconFile $icon
 
-	if($thumbprint -ne '')
-	{
-		write-host " -> signing manifest file"
-		mage -Sign $manifestFileName -CertHash $thumbprint
-	}
-	else
-	{
-		write-host ' -> !!Skipping signing, no hash cert'
-	}
+  # write-host ' -> Add file associations to manfest'
+  # $fullPath = resolve-path($manifestFileName)
+  # $doc = [xml](Get-Content -Path $fullPath)
+  # $association = $doc.CreateElement('fileAssociation')
+  # $association.SetAttribute('xmlns','urn:schemas-microsoft-com:clickonce.v1')
+  # $association.SetAttribute('extension','.ext')
+  # $association.SetAttribute('description','Decription')
+  # $association.SetAttribute('progid','YourApp.Document')
+  # $association.SetAttribute('defaultIcon', $icon)
+  # $doc.assembly.AppendChild($association) > $null
+  # $doc.Save($fullPath)
 
-	# rename all files to .deploy
-	gci -exclude *.manifest -r $targetVersion | where { $_.PSIsContainer -eq $false } | rename-item -newname { $_.name + ".deploy" }
+  if($thumbprint -ne '')
+  {
+    write-host " -> signing manifest file"
+    mage -Sign $manifestFileName -CertHash $thumbprint
+  }
+  else
+  {
+    write-host ' -> !!Skipping signing, no hash cert'
+  }
 
-	write-host "creating deployment manifest"
+  # rename all files to .deploy
+  gci -exclude *.manifest -r $targetVersion | where { $_.PSIsContainer -eq $false } | rename-item -newname { $_.name + ".deploy" }
 
-	$applicationFileName = join-path $target "$applicationExecutable.application"
-	$providerUrl = join-path $providerPath "$applicationExecutable.application"
-    mage -New Deployment `
-		 -Processor x86 `
-		 -Install true `
-		 -Publisher $publisher `
-		 -ProviderUrl $providerUrl `
-		 -AppManifest $manifestFileName `
-		 -Version $version `
-		 -Name $applicationDisplayName `
-		 -ToFile $applicationFileName `
-		 -UseManifestForTrust true
+  write-host "creating deployment manifest"
 
-	$fullPath = resolve-path($applicationFileName)
-	$doc = [xml](Get-Content -Path $fullPath)
-	
-	write-host ' -> Enabling trust url parameters'
-	$doc.assembly.deployment.SetAttribute("trustURLParameters", "true")
-	
-	write-host ' -> Enabling map file extensions'
-	$doc.assembly.deployment.SetAttribute("mapFileExtensions", "true")
-	$doc.Save($fullPath)
+  $applicationFileName = join-path $target "$applicationExecutable.application"
+  $providerUrl = "$providerPath$applicationExecutable.application"
+  
+  mage -New Deployment `
+    -Processor x86 `
+    -Install true `
+    -Publisher $publisher `
+    -ProviderUrl $providerUrl `
+    -AppManifest $manifestFileName `
+    -Version $version `
+    -Name $applicationDisplayName `
+    -ToFile $applicationFileName `
+    -UseManifestForTrust true
 
-	write-host ' -> Updating application with MinVersion'
-	mage -Update $applicationFileName -MinVersion $version
+  $fullPath = resolve-path($applicationFileName)
+  $doc = [xml](Get-Content -Path $fullPath)
+  
+  write-host ' -> Enabling trust url parameters'
+  $doc.assembly.deployment.SetAttribute("trustURLParameters", "true")
+  
+  write-host ' -> Enabling map file extensions'
+  $doc.assembly.deployment.SetAttribute("mapFileExtensions", "true")
+  $doc.Save($fullPath)
 
-	write-host ' -> Changing expiration max age => before application start up'
-	$content = Get-Content $fullPath
-	$content -replace "<expiration maximumAge=`"0`" unit=`"days`" />", "<beforeApplicationStartup />" | set-content $fullPath
+  write-host ' -> Updating application with MinVersion'
+  mage -Update $applicationFileName -MinVersion $version
 
-	if($thumbprint -ne '')
-	{
-		write-host " -> signing .application"
-		mage -Update $applicationFileName -CertHash $thumbprint
-	}
-	else
-	{
-		write-host ' -> !!Skipping signing, no hash cert'
-	}
+  write-host ' -> Changing expiration max age => before application start up'
+  $content = Get-Content $fullPath
+  $content -replace "<expiration maximumAge=`"0`" unit=`"days`" />", "<beforeApplicationStartup />" | set-content $fullPath
 
-	write-host 'successfully created click once application'
+  if($thumbprint -ne '')
+  {
+    write-host " -> signing .application"
+    mage -Update $applicationFileName -CertHash $thumbprint
+  }
+  else
+  {
+    write-host ' -> !!Skipping signing, no hash cert'
+  }
+
+  write-host 'successfully created click once application'
 }
