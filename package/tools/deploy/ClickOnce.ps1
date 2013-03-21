246aabf0610ec92b81435af0d6895cadcb0c1446
jonnii <me@jonnii.com>
2013-03-21 11:32:28 -0400

    add cmdlet binding use write-verbose, not write-host.

--- package/tools/deploy/ClickOnce.ps1
+++ package/tools/deploy/ClickOnce.ps1
@@ -1,4 +1,5 @@
 function Prepare-ClickOnce() {
+  [CmdLetBinding()]
   param(
     [Parameter(Mandatory=$true)] [string] $target,
     [Parameter(Mandatory=$true)] [string] $version,
@@ -15,26 +16,26 @@ function Prepare-ClickOnce() {
     [string] $magePath = "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools"
   )
 
-  Write-Host "Preparing click once"
-  Write-Host " -> Target: $target"
-  Write-Host " -> Version: $version"
-  Write-Host " -> Source: source"
-  Write-Host " -> Application Executable: $applicationExecutable"
-  Write-Host " -> Application Name: $applicationName"
-  Write-Host " -> Application Display Name: $applicationDisplayName"
-  Write-Host " -> Icon: $icon"
-  Write-Host " -> Publisher: $publisher"
-  Write-Host " -> ProviderPath: $providerPath"
-  Write-Host " -> Thumbprint: $thumbprint"
-  Write-Host " -> Assembly Identity Name: $assemblyIdentityName"
-  Write-Host " -> Processor: $processor"
+  Write-Verbose "Preparing click once"
+  Write-Verbose " -> Target: $target"
+  Write-Verbose " -> Version: $version"
+  Write-Verbose " -> Source: source"
+  Write-Verbose " -> Application Executable: $applicationExecutable"
+  Write-Verbose " -> Application Name: $applicationName"
+  Write-Verbose " -> Application Display Name: $applicationDisplayName"
+  Write-Verbose " -> Icon: $icon"
+  Write-Verbose " -> Publisher: $publisher"
+  Write-Verbose " -> ProviderPath: $providerPath"
+  Write-Verbose " -> Thumbprint: $thumbprint"
+  Write-Verbose " -> Assembly Identity Name: $assemblyIdentityName"
+  Write-Verbose " -> Processor: $processor"
 
   # Add mage to our path
   $env:path += ";$magePath"
 
-  Write-Host "Preparing install directory"
+  Write-Verbose "Preparing install directory"
   if (test-path $target) {
-    Write-Host "Cleaning target directory"
+    Write-Verbose "Cleaning target directory"
     rm -r -force $target > $null
   }
   mkdir $target > $null
@@ -43,12 +44,12 @@ function Prepare-ClickOnce() {
   
   # create an install version directory
   mkdir $targetVersion
-  write-host "Prepare install directory for version $version"
+  Write-Verbose "Prepare install directory for version $version"
 
   cp -r -exclude *.xml "$source\*" $targetVersion
   rm "$targetVersion\*vshost*"
 
-  write-host 'Creating manifest file'
+  Write-Verbose 'Creating manifest file'
   $manifestFileName = join-path $targetVersion "$applicationExecutable.exe.manifest"
   mage -New Application `
      -Processor $processor `
@@ -66,7 +67,7 @@ function Prepare-ClickOnce() {
   # so we have to reach into the manifest to update it
 
   if($assemblyIdentityName) {
-    Write-Host "Applying override for  $assemblyIdentityName"
+    Write-Verbose "Applying override for  $assemblyIdentityName"
     $doc.assembly.assemblyIdentity.SetAttribute("name", $assemblyIdentityName)
     $doc.Save($manifestPath)
   }
@@ -81,18 +82,18 @@ function Prepare-ClickOnce() {
   
   if($thumbprint -ne '')
   {
-    write-host " -> signing manifest file"
+    Write-Verbose " -> signing manifest file"
     mage -Sign $manifestFileName -CertHash $thumbprint
   }
   else
   {
-    write-host ' -> !!Skipping signing, no hash cert'
+    Write-Verbose ' -> !!Skipping signing, no hash cert'
   }
 
   # rename all files to .deploy
   gci -exclude *.manifest -r $targetVersion | where { $_.PSIsContainer -eq $false } | rename-item -newname { $_.name + ".deploy" }
 
-  write-host "creating deployment manifest"
+  Write-Verbose "creating deployment manifest"
 
   $applicationFileName = join-path $target "$applicationExecutable.application"
   $providerUrl = "$providerPath$applicationExecutable.application"
@@ -111,29 +112,29 @@ function Prepare-ClickOnce() {
   $fullPath = resolve-path($applicationFileName)
   $doc = [xml](Get-Content -Path $fullPath)
   
-  write-host ' -> Enabling trust url parameters'
+  Write-Verbose ' -> Enabling trust url parameters'
   $doc.assembly.deployment.SetAttribute("trustURLParameters", "true")
   
-  write-host ' -> Enabling map file extensions'
+  Write-Verbose ' -> Enabling map file extensions'
   $doc.assembly.deployment.SetAttribute("mapFileExtensions", "true")
   $doc.Save($fullPath)
 
-  write-host ' -> Updating application with MinVersion'
+  Write-Verbose ' -> Updating application with MinVersion'
   mage -Update $applicationFileName -MinVersion $version
 
-  write-host ' -> Changing expiration max age => before application start up'
+  Write-Verbose ' -> Changing expiration max age => before application start up'
   $content = Get-Content $fullPath
   $content -replace "<expiration maximumAge=`"0`" unit=`"days`" />", "<beforeApplicationStartup />" | set-content $fullPath
 
   if($thumbprint -ne '')
   {
-    write-host " -> signing .application"
+    Write-Verbose " -> signing .application"
     mage -Update $applicationFileName -CertHash $thumbprint
   }
   else
   {
-    write-host ' -> !!Skipping signing, no hash cert'
+    Write-Verbose ' -> !!Skipping signing, no hash cert'
   }
 
-  write-host 'successfully created click once application'
+  Write-Verbose 'successfully created click once application'
 }
